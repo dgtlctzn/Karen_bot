2746a8322d874e796e9466ec608c2688f1df9ea1
Joseph Perry <josephperry720@gmail.com>
2020-07-19 15:48:49 -0400

    started karen class

--- Karen_bot3.py
+++ Karen_bot3.py
@@ -48,74 +48,78 @@ class MarkovTheWise:
         return text_model
 
 
-def bad_review(review_text, structure_text, chains, weight=(1, 10)):
-    hashtags = [' #disappointing', ' #crap', ' #nevercomingback', ' #theworst', ' #livelaughlove...andleave',
-                ' #neveragain', ' #unacceptable', ' #badservice', ' #takemybusinesselsewhere', ' #outrageous']
-    hash_num_1 = randint(0, 9)
-    hash_one = hashtags[hash_num_1]
-    hash_two = hashtags[hash_num_1 - randint(1, 3)]
+class Karen(TweetBot, MarkovTheWise):
 
-    km = MarkovTheWise(review_text)
-    mm = MarkovTheWise(structure_text)
+    def __init__(self, file):
+        super().__init__(self, file)
 
-    my_model = km.make_model()
-    my_model_2 = mm.make_model()
+    def bad_review(self, review_text, structure_text, chains, weight=(1, 10)):
+        hashtags = [' #disappointing', ' #crap', ' #nevercomingback', ' #theworst', ' #livelaughlove...andleave',
+                    ' #neveragain', ' #unacceptable', ' #badservice', ' #takemybusinesselsewhere', ' #outrageous']
+        hash_num_1 = randint(0, 9)
+        hash_one = hashtags[hash_num_1]
+        hash_two = hashtags[hash_num_1 - randint(1, 3)]
 
-    combo_model = markovify.combine([my_model, my_model_2], weight)
+        km = MarkovTheWise(review_text)
+        mm = MarkovTheWise(structure_text)
 
-    restaurant_sent = combo_model.make_short_sentence(80, min_chars=60, tries=100)
-    my_tweet = "I'd like to speak to the manager. " + chains[randint(1, 8)] + ' ' + restaurant_sent + hash_one \
-               + hash_two + ' #karen' + ' #karensgonewild'
-    return my_tweet
+        my_model = km.make_model()
+        my_model_2 = mm.make_model()
 
+        combo_model = markovify.combine([my_model, my_model_2], weight)
 
-def get_recipe(url):
-    my_url = url
+        restaurant_sent = combo_model.make_short_sentence(80, min_chars=60, tries=100)
+        my_tweet = "I'd like to speak to the manager. " + chains[randint(1, 8)] + ' ' + restaurant_sent + hash_one \
+                   + hash_two + ' #karen' + ' #karensgonewild'
+        return my_tweet
 
-    page = requests.get(my_url)
+    def get_recipe(self, url):
+        my_url = url
 
-    soup = bs4.BeautifulSoup(page.content, 'html.parser')
-    results = soup.find_all('div', class_='fixed-recipe-card__info')
+        page = requests.get(my_url)
 
-    web_dict = {i: str(results[i]).split('href="')[1].split('">')[0] for i in range(0, len(results))}
-    return web_dict
+        soup = bs4.BeautifulSoup(page.content, 'html.parser')
+        results = soup.find_all('div', class_='fixed-recipe-card__info')
 
+        web_dict = {i: str(results[i]).split('href="')[1].split('">')[0] for i in range(0, len(results))}
+        return web_dict
 
-def main():
-    number = randint(1, 4)
-    if number == 1:
-        chains = {1: "@Chilis", 2: "@olivegarden", 3: "@Applebees", 4: '@Cheesecake', 5: "@Carrabbas",
-                  6: "@PFChangs", 7: "@Maggianos", 8: "@redlobster"}
-        text_1 = '/home/pi/PiPy/Karen_text2.txt'
-        text_2 = '/home/pi/PiPy/Karen_rest_structure.txt'
-        tb = TweetBot()
-        tb.post(bad_review(text_1, text_2, chains))
-    elif number == 2:
-        chains = {1: "@Sephora", 2: "@Target", 3: "@jcpenney", 4: "@tjmaxx", 5: "@marshalls",
-                  6: "@Walmart", 7: "@Bloomingdales", 8: "@Macys"}
-        text_1 = '/home/pi/PiPy/Karen_retail2.txt'
-        text_2 = '/home/pi/PiPy/Karen_retail_structure.txt'
-        tb = TweetBot()
-        tb.post(bad_review(text_1, text_2, chains))
-    elif number == 3:
-        user_list = [31869934, 22650211, 2370627199, 26031542, 22515362]
-        user_number = randint(0, 4)
-        tb = TweetBot()
-        tb.re_tweet(user_list[user_number])
-    else:
-        recipe_site_num = randint(0, 2)
-        recipe_type = ['https://www.allrecipes.com/search/results/?ingIncl=ranch&sort=re',
-                       'https://www.allrecipes.com/search/results/?ingIncl=bacon&sort=re',
-                       'https://www.allrecipes.com/search/results/?wt=potato%20salad&sort=re']
-        current_dict = get_recipe(recipe_type[recipe_site_num])
-        recipe_num = randint(0, len(current_dict) - 1)
-        share_recipe = current_dict[recipe_num]
-        tb = TweetBot()
-        tb.post('YUM!' + share_recipe.strip("'"))
+    def complain(self):
+        number = randint(1, 4)
+        if number == 1:
+            chains = {1: "@Chilis", 2: "@olivegarden", 3: "@Applebees", 4: '@Cheesecake', 5: "@Carrabbas",
+                      6: "@PFChangs", 7: "@Maggianos", 8: "@redlobster"}
+            text_1 = '/home/pi/PiPy/Karen_text2.txt'
+            text_2 = '/home/pi/PiPy/Karen_rest_structure.txt'
+            tb = TweetBot()
+            tb.post(bad_review(text_1, text_2, chains))
+        elif number == 2:
+            chains = {1: "@Sephora", 2: "@Target", 3: "@jcpenney", 4: "@tjmaxx", 5: "@marshalls",
+                      6: "@Walmart", 7: "@Bloomingdales", 8: "@Macys"}
+            text_1 = '/home/pi/PiPy/Karen_retail2.txt'
+            text_2 = '/home/pi/PiPy/Karen_retail_structure.txt'
+            tb = TweetBot()
+            tb.post(bad_review(text_1, text_2, chains))
+        elif number == 3:
+            user_list = [31869934, 22650211, 2370627199, 26031542, 22515362]
+            user_number = randint(0, 4)
+            tb = TweetBot()
+            tb.re_tweet(user_list[user_number])
+        else:
+            recipe_site_num = randint(0, 2)
+            recipe_type = ['https://www.allrecipes.com/search/results/?ingIncl=ranch&sort=re',
+                           'https://www.allrecipes.com/search/results/?ingIncl=bacon&sort=re',
+                           'https://www.allrecipes.com/search/results/?wt=potato%20salad&sort=re']
+            current_dict = get_recipe(recipe_type[recipe_site_num])
+            recipe_num = randint(0, len(current_dict) - 1)
+            share_recipe = current_dict[recipe_num]
+            tb = TweetBot()
+            tb.post('YUM!' + share_recipe.strip("'"))
 
 
 try:
-    main()
+    Karen()
+    complain()
 except twitter.error.TwitterError:
     back_up = {1: 'God bless the troops and the law enforcement keeping us safe! #america',
                2: 'Living, Laughing, and Loving Life #blessed',
